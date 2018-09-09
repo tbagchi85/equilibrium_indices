c5a0593906d1ec94d3bcd1f169ced854843ed481
Tanmay Bagchi <tbagchi85@gmail.com>
2018-09-09 13:58:07 +0530

    sum of first n elements

--- lib/equilibrium_indices.rb
+++ lib/equilibrium_indices.rb
@@ -2,4 +2,10 @@ class EquilibriumIndices
   def self.find_indices(array=[])
     return [] if array.length <= 2
   end
+
+  protected
+
+  def self.sum_of_first_n(array, index)
+    array.first(index).sum
+  end
 end

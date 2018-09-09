cab96400dc35342256b1f292c4a2f5d7de0a5695
Tanmay Bagchi <tbagchi85@gmail.com>
2018-09-09 19:37:53 +0530

    returns equilibrium indices for multiple array elements

--- lib/equilibrium_indices.rb
+++ lib/equilibrium_indices.rb
@@ -1,6 +1,16 @@
 class EquilibriumIndices
   def self.find_indices(array=[])
+    indices = []
+
     return [] if array.length <= 2
+
+    for i in 0..(array.length - 1)
+      if sum_of_first_n(array, i) ==  sum_of_last_n(array, array.length - (i + 1))
+      	indices << i
+      end
+    end
+
+    indices
   end
 
   protected
